89876aeb9c4d51a4e31873660605b96dd729c195
Camilo Tobón Florez <camilotobon21@gmail.com>
2020-05-11 15:35:08 -0500

    Primer commit

---
+++ utils/mocks/products.js
@@ -0,0 +1,16 @@
+const productMocks = [
+    {
+      key: 1,
+      name: "Red shoes",
+      price: 75,
+      image: "public/images/tenis.jpg"
+    },
+    {
+      key: 2,
+      name: "Black bike",
+      price: 300,
+      image: "public/images/bike.jpg"
+    }
+  ];
+
+  module.exports = productMocks;
